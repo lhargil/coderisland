74219a20be4c4ffdd416dc9c8c98c763412e9938
Lhar Gil <gil_lhar@live.com>
2020-09-13 17:03:10 +0800

    feat: custom forms styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,90 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
 module.exports = (isProd) => ({
-    prefix: '',
-    future: {
-      removeDeprecatedGapUtilities: true,
-      purgeLayersByDefault: true
-    },
-    purge: {
-      enabled: isProd,
-      content: ['**/*.html', '**/*.ts']
-    },
-    theme: {
+  prefix: '',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: isProd,
+    content: ['**/*.html', '**/*.ts'],
+  },
+  theme: {
+    extend: {
       fontFamily: {
-          display: ['Raleway', ...defaultTheme.fontFamily.sans],
-          body: ['Roboto', ...defaultTheme.fontFamily.sans],
+        display: ['Raleway', ...defaultTheme.fontFamily.sans],
+        body: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      customForms: (theme) => ({
+        invalid: {
+          'multiselect, checkbox, radio': {
+            'border-bottom-color': theme('colors.red.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+            '&::placeholder': {
+              color: theme('colors.red.300'),
+            },
+            '&:focus': {
+              boxShadow: theme('boxShadow.outline-red'),
+              borderColor: theme('colors.red.300'),
+              'border-bottom-width': theme('borderWidth.2'),
+            },
+          },
         },
+        valid: {
+          multiselect: {
+            'border-bottom-color': theme('colors.green.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+          },
+        },
+      }),
     },
-    plugins: [
-      require('@tailwindcss/ui')({
-        layout: 'sidebar',
-      })
-    ]
+  },
+  variants: {
+    borderWidth: [
+      'responsive',
+      'first',
+      'last',
+      'odd',
+      'even',
+      'hover',
+      'focus',
+    ],
+    backgroundColor: ['responsive', 'odd', 'even', 'hover', 'focus'],
+    translate: ['responsive', 'hover', 'focus'],
+    margin: ['responsive', 'hover', 'focus', 'last'],
+    padding: ['responsive', 'hover', 'focus', 'last'],
+    placeholderColor: ['responsive', 'focus', 'hover', 'active'],
+    boxShadow: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+  },
+  plugins: [
+    require('@tailwindcss/ui')({
+      layout: 'sidebar',
+    }),
+    plugin(function ({ addComponents, theme, config }) {
+      const formControlStyles = {
+        '.control-invalid': {
+          '& input, & textarea, & select': {
+            'border-bottom-color': theme('colors.red.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+            '&::placeholder': {
+              color: theme('colors.red.300'),
+            },
+            '&:focus': {
+              boxShadow: theme('boxShadow.outline-red'),
+              borderColor: theme('colors.red.300'),
+              'border-bottom-width': theme('borderWidth.2'),
+            },
+          },
+        },
+        '.control-valid': {
+          '& input, & textarea, & select': {
+            'border-bottom-color': theme('colors.green.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+          },
+        },
+      };
+      addComponents(formControlStyles);
+    }),
+  ],
 });
