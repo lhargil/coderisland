aadbc3c71fc93179aca30a947838b865d82d98ac
Lhar Gil <gil_lhar@live.com>
2020-09-13 15:18:39 +0800

    initial app setup

---
+++ apps/resume-builder/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: 'resumes',
+          loadChildren: () =>
+            import('@coderisland/resume-builder/resume/shell-resume').then(
+              (module) => module.ResumeBuilderResumeShellResumeModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabled' }
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ libs/resume-builder/resume/shell-resume/README.md
@@ -0,0 +1,7 @@
+# resume-builder-resume-shell-resume
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test resume-builder-resume-shell-resume` to execute the unit tests.
