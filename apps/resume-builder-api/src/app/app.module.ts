2df87ed4784b109bd24c2d9faff3734ff07b5244
Lhar Gil <gil_lhar@live.com>
2020-11-14 17:10:01 +0800

    refactor: set mongodb uri

--- apps/resume-builder-api/src/app/app.module.ts
+++ apps/resume-builder-api/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { environment } from '../environments/environment';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('mongoDbUri'),
+        uri: process.env.CEEVEE_MONGODB_URI
       }),
       inject: [ConfigService],
     }),
