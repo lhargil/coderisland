e835726dd197754b479b69588fdc106065e8aa4a
Lhar Gil <gil_lhar@live.com>
2020-12-02 11:16:14 +0800

    chore(deps): update to nx@10.4

--- libs/ui-kit/forms/text-input/jest.config.js
+++ libs/ui-kit/forms/text-input/jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  name: 'ui-kit-forms-text-input',
-  preset: '../../../../jest.config.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
     },
   },
   coverageDirectory: '../../../../coverage/libs/ui-kit/forms/text-input',
@@ -18,4 +19,5 @@ module.exports = {
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
+  displayName: 'ui-kit-forms-text-input',
 };
