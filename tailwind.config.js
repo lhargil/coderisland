18da2af0771e5a6f5d8b7c4dbf13f7d481aacff2
Lhar Gil <gil_lhar@live.com>
2020-12-13 15:03:50 +0800

    chore(configs): moved resume-builder tailwind configs to separate dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,85 +25,14 @@ module.exports = (isProd) => ({
         current: 'currentColor',
         black: '#000',
         white: '#fff',
-        cyan: colors.cyan,
-        'cool-gray': colors.coolGray,
       },
-      screens: {
-        print: { raw: 'print' },
-      },
-      fontFamily: {
-        display: ['Raleway', ...defaultTheme.fontFamily.sans],
-        body: ['Roboto', ...defaultTheme.fontFamily.sans],
-      },
-      customForms: (theme) => ({
-        invalid: {
-          'multiselect, checkbox, radio': {
-            'border-bottom-color': theme('colors.red.300'),
-            'border-bottom-width': theme('borderWidth.2'),
-            '&::placeholder': {
-              color: theme('colors.red.300'),
-            },
-            '&:focus': {
-              boxShadow: theme('boxShadow.outline-red'),
-              borderColor: theme('colors.red.300'),
-              'border-bottom-width': theme('borderWidth.2'),
-            },
-          },
-        },
-        valid: {
-          multiselect: {
-            'border-bottom-color': theme('colors.green.300'),
-            'border-bottom-width': theme('borderWidth.2'),
-          },
-        },
-      }),
     },
   },
   variants: {
-    borderWidth: [
-      'responsive',
-      'first',
-      'last',
-      'odd',
-      'even',
-      'hover',
-      'focus',
-    ],
-    backgroundColor: ['responsive', 'odd', 'even', 'hover', 'focus'],
-    translate: ['responsive', 'hover', 'focus'],
-    margin: ['responsive', 'hover', 'focus', 'last', 'first'],
-    padding: ['responsive', 'hover', 'focus', 'last'],
-    placeholderColor: ['responsive', 'focus', 'hover', 'active'],
-    boxShadow: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-    plugin(function ({ addComponents, theme, config }) {
-      const formControlStyles = {
-        '.control-invalid': {
-          '& input, & textarea, & select': {
-            'border-bottom-color': theme('colors.red.300'),
-            'border-bottom-width': theme('borderWidth.2'),
-            '&::placeholder': {
-              color: theme('colors.red.300'),
-            },
-            '&:focus': {
-              boxShadow: theme('boxShadow.outline-red'),
-              borderColor: theme('colors.red.300'),
-              'border-bottom-width': theme('borderWidth.2'),
-            },
-          },
-        },
-        '.control-valid': {
-          '& input, & textarea, & select': {
-            'border-bottom-color': theme('colors.green.300'),
-            'border-bottom-width': theme('borderWidth.2'),
-          },
-        },
-      };
-      addComponents(formControlStyles);
-    }),
   ],
 });
