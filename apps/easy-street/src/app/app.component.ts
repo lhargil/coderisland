86577ef4ab02e9a6e66c8bfbbfdb4852ac07caed
Lhar Gil <gil_lhar@live.com>
2020-12-13 16:36:19 +0800

    chore: initial commit

---
+++ apps/easy-street/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'easy-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'easy-street';
+}
---
+++ tailwind-configs/easy-street.tailwind.config.js
@@ -0,0 +1,109 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+const colors = require('tailwindcss/colors');
+
+module.exports = (isProd) => ({
+  prefix: '',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: isProd,
+    content: [
+      './apps/?(easy-street)/**/*.html',
+      './apps/?(easy-street)/**/*.ts',
+      './libs/+({ui-kit,shared})/**/*.html',
+      './libs/+({ui-kit,shared})/**/*.ts',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000',
+        white: '#fff',
+        purple: colors.violet,
+        'cool-gray': colors.coolGray,
+      },
+      screens: {
+        print: { raw: 'print' },
+      },
+      fontFamily: {
+        display: ['Raleway', ...defaultTheme.fontFamily.sans],
+        body: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      customForms: (theme) => ({
+        invalid: {
+          'multiselect, checkbox, radio': {
+            'border-bottom-color': theme('colors.red.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+            '&::placeholder': {
+              color: theme('colors.red.300'),
+            },
+            '&:focus': {
+              boxShadow: theme('boxShadow.outline-red'),
+              borderColor: theme('colors.red.300'),
+              'border-bottom-width': theme('borderWidth.2'),
+            },
+          },
+        },
+        valid: {
+          multiselect: {
+            'border-bottom-color': theme('colors.green.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    borderWidth: [
+      'responsive',
+      'first',
+      'last',
+      'odd',
+      'even',
+      'hover',
+      'focus',
+    ],
+    backgroundColor: ['responsive', 'odd', 'even', 'hover', 'focus'],
+    translate: ['responsive', 'hover', 'focus'],
+    margin: ['responsive', 'hover', 'focus', 'last', 'first'],
+    padding: ['responsive', 'hover', 'focus', 'last'],
+    placeholderColor: ['responsive', 'focus', 'hover', 'active'],
+    boxShadow: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    plugin(function ({ addComponents, theme, config }) {
+      const formControlStyles = {
+        '.control-invalid': {
+          '& input, & textarea, & select': {
+            'border-bottom-color': theme('colors.red.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+            '&::placeholder': {
+              color: theme('colors.red.300'),
+            },
+            '&:focus': {
+              boxShadow: theme('boxShadow.outline-red'),
+              borderColor: theme('colors.red.300'),
+              'border-bottom-width': theme('borderWidth.2'),
+            },
+          },
+        },
+        '.control-valid': {
+          '& input, & textarea, & select': {
+            'border-bottom-color': theme('colors.green.300'),
+            'border-bottom-width': theme('borderWidth.2'),
+          },
+        },
+      };
+      addComponents(formControlStyles);
+    }),
+  ],
+});
