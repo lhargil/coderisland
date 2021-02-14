15f047c7caa44bdf02b1e6991b818912123d19db
Lhar Gil <devlana@outlook.com>
2021-02-14 10:39:17 +0800

    chore: removed photos shell reference

--- apps/omgimgflow/src/app/app.module.ts
+++ apps/omgimgflow/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { OmgimgflowPhotosShellModule } from '@coderisland/omgimgflow/photos/shell';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    OmgimgflowPhotosShellModule
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent],
