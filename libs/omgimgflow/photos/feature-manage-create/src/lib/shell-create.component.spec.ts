8ee4d835bf785b673d0ecb9209c55cce55196980
Lhar Gil <devlana@outlook.com>
2021-02-16 23:01:07 +0800

    test: added deps in shell-create spec

--- libs/omgimgflow/photos/feature-manage-create/src/lib/shell-create.component.spec.ts
+++ libs/omgimgflow/photos/feature-manage-create/src/lib/shell-create.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ShellCreateComponent } from './shell-create.component';
+
+import { UiEditFormModule } from '@coderisland/omgimgflow/photos/shared/ui-edit-form';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ShellCreateComponent', () => {
   let component: ShellCreateComponent;
@@ -8,6 +13,7 @@ describe('ShellCreateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [UiEditFormModule, ReactiveFormsModule, HttpClientModule, RouterTestingModule],
       declarations: [ ShellCreateComponent ]
     })
     .compileComponents();
