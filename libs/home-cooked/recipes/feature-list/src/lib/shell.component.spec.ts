3808b32c55cbde9a7709e465beceb174551c1ca9
Lhar Gil <gil_lhar@live.com>
2021-02-28 18:37:29 +0800

    test: fix error in shell component spec

--- libs/home-cooked/recipes/feature-list/src/lib/shell.component.spec.ts
+++ libs/home-cooked/recipes/feature-list/src/lib/shell.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ListComponent } from './list.component';
 
 import { ShellComponent } from './shell.component';
@@ -10,7 +11,7 @@ describe('ShellComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ ShellComponent, ListComponent ]
     })
     .compileComponents();
