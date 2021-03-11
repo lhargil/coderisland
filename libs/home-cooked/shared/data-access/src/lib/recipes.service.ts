c0dd98cffe088c29f8d6736cdbe696b8a053219f
Lhar Gil <gil_lhar@live.com>
2021-03-11 20:51:26 +0800

    refactor: updated recipe images path

--- libs/home-cooked/shared/data-access/src/lib/recipes.service.ts
+++ libs/home-cooked/shared/data-access/src/lib/recipes.service.ts
@@ -2,19 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Recipe } from '@coderisland/home-cooked/shared/models';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipesService {
-
   constructor(private readonly httpClient: HttpClient) {}
 
   getAll(): Observable<Recipe[]> {
-    return this.httpClient.get<Recipe[]>(`api/recipes`);
+    return this.httpClient.get<Recipe[]>(`api/recipes`).pipe(
+      map((recipes: Recipe[]) => {
+        return recipes.map((recipe: Recipe) => ({
+          ...recipe,
+          recipeImage: `${recipe.recipeImage}?crop=10,10,-10,-50`,
+        }));
+      }),
+    );
   }
 
   getOne(id: string): Observable<Recipe> {
-    return this.httpClient.get<Recipe>(`api/recipes/${id}`);
+    return this.httpClient
+      .get<Recipe>(`api/recipes/${id}`)
+      .pipe(map((recipe: Recipe) => ({ ...recipe, recipeImage: `${recipe.recipeImage}?crop=10,10,-10,-50` })));
   }
 }
