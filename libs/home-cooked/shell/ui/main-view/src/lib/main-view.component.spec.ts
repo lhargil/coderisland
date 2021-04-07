666f98a49eb85ae16ba797dcb456e2b98ec9f379
Lhar Gil <gil_lhar@live.com>
2021-04-07 22:30:04 +0800

    test: added deps on main view

--- libs/home-cooked/shell/ui/main-view/src/lib/main-view.component.spec.ts
+++ libs/home-cooked/shell/ui/main-view/src/lib/main-view.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainViewComponent } from './main-view.component';
+import { RouterTestingModule} from '@angular/router/testing';
 
 describe('MainViewComponent', () => {
   let component: MainViewComponent;
@@ -8,6 +9,7 @@ describe('MainViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ MainViewComponent ]
     })
     .compileComponents();
